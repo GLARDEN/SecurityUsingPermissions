39941cdc4ab3c7ec0ace57bf3f4969183d8f348c
Greg <GregLarden@gmail.com>
2022-04-04 17:15:41 -0400

    revert change testing github connection

--- BlazorClient/Pages/Login.razor.cs
+++ BlazorClient/Pages/Login.razor.cs
@@ -19,7 +19,7 @@ public partial class Login : ComponentBase
     private string returnURL = string.Empty;
 
     protected override void OnInitialized()
-    {
+    {   
         var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
         if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("returnUrl", out var url))
         {
